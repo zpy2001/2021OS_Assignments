8996129fb77b127e8b47bd4d08fb0b88897c49cd
zpy2001 <zpy19260817@gmail.com>
2021-12-31 14:29:42 +0800

    finish 2022

--- size_of_int/main.c
+++ size_of_int/main.c
@@ -3,36 +3,54 @@
 typedef struct {
     char c;
     short s;
-    double d;
-}struct_t;
-
-typedef union {
-    char c;
     int i;
-}union_t;
-int main() {
+    long l;
+    float f;
+    double d;
+    long double ld;
+}struct_t1;
+
+typedef struct {
     char c;
     short s;
     int i;
     long l;
-    long long ll;
     float f;
     double d;
-    long double ld;
-    void * v;
-    struct_t str;
-    union_t uni;
-    printf("%lu\n", sizeof (c));
-    printf("%lu\n", sizeof (s));
-    printf("%lu\n", sizeof (i));
-    printf("%lu\n", sizeof (l));
-    printf("%lu\n", sizeof (ll));
-    printf("%lu\n", sizeof (f));
-    printf("%lu\n", sizeof (d));
-    printf("%lu\n", sizeof (ld));
-    printf("%lu\n", sizeof (v));
-    printf("%lu\n", sizeof (str));
-    printf("%lu\n", sizeof (uni));
+}struct_t2;
+
+typedef struct {
+    char c;
+    short s;
+    int i;
+    double d;
+}struct_t3;
+
+typedef struct {
+    char c;
+    int i;
+}struct_t4;
+
+typedef struct {
+    long i;
+    long d;
+    long c;
+    long s;
+}struct_t5;
+
+int main() {
+
+    struct_t1 str1;
+    struct_t2 str2;
+    struct_t3 str3;
+    struct_t4 str4;
+    struct_t5 str5;
+
+    printf("%lu\n", sizeof (str1));
+    printf("%lu\n", sizeof (str2));
+    printf("%lu\n", sizeof (str3));
+    printf("%lu\n", sizeof (str4));
+    printf("%lu\n", sizeof (str5));
 
     return 0;
 }
